409459661d112bb01802be42f708f5aaa1ba1452
JavierVillalobosC <javi.nicovill@gmail.com>
2023-10-29 12:28:33 -0300

    corrección error rutas

---
+++ backend/src/routes/pagos.routes.js
@@ -0,0 +1,21 @@
+"use strict"
+const express = require("express");
+
+const pagoController = require("../controllers/pagos.controller.js");
+
+const authorizationMiddleware = require("../middlewares/authorization.middleware.js");
+
+
+const authenticationMiddleware = require("../middlewares/authentication.middleware.js");
+
+const router = express.Router();
+
+router.use(authenticationMiddleware);
+
+router.get("/", pagoController.getPagos);
+router.post("/", authorizationMiddleware.isAdmin, pagoController.createPago);
+router.get("/:id", pagoController.getPagoById);
+router.put("/:id", authorizationMiddleware.isAdmin, pagoController.updatePago);
+router.delete("/:id", authorizationMiddleware.isAdmin, pagoController.deletePago);
+
+module.exports = router;
