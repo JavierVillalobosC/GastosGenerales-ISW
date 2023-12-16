ef58dfe8fce5b3cc695902e5f0b52315c5a950d1
Vicente Sanhueza Muñoz <vicente.sanhueza1901@alumnos.ubiobio.cl>
2023-12-16 15:17:02 -0300

    Interes update v2

--- backend/src/services/interes.service.js
+++ backend/src/services/interes.service.js
@@ -48,14 +48,16 @@ async function crearInteres() {
                 const pay = await Pay.findOne({ debt: debt._id }).exec();
 
                 // Verifica que el pago exista
-                if (!pay) {
+                /* if (!pay) {
                     console.log(`No se encontró un pago para la deuda con id ${debt._id}`);
                     continue; // Salta al siguiente ciclo del bucle
-                }
+                } */
 
                 // Determina el porcentaje de interés basado en el tipo de pago
                 let porcentajeInteres;
-                if (pay.type === 'total') {
+                if (!pay) {
+                    porcentajeInteres = 0.15; // Por ejemplo, 2% para deudas no pagadas
+                } else if (pay.type === 'total') {
                     porcentajeInteres = 0.05; // Por ejemplo, 5% para pagos totales
                 } else if (pay.type === 'parcial') {
                     porcentajeInteres = 0.10; // Por ejemplo, 10% para pagos parciales
@@ -67,6 +69,12 @@ async function crearInteres() {
                 // Calcula el interés basado en la deuda del usuario
                 const interes = user.debt * porcentajeInteres;
 
+                // Calcula el valor final de la deuda con el interés agregado
+                const valorFinal = user.debt + interes;
+
+                // Muestra un mensaje en la consola
+                console.log(`> Se aplicó un interés del ${porcentajeInteres * 100}% a la deuda con ID ${debt._id}.\nEl interés es de ${interes}. \nEl valor final de la deuda con el interés agregado es ${valorFinal}.`);
+
                 // Crea un nuevo interés
                 const newInterest = new Interest({
                     interesID: new mongoose.Types.ObjectId().toString(),
@@ -76,6 +84,14 @@ async function crearInteres() {
 
                 // Guarda el nuevo interés en la base de datos
                 await newInterest.save();
+                // Actualiza el valor 'amount' de la deuda con el nuevo valor de la deuda más el interés
+                await Debt.updateOne({ _id: debt._id }, { amount: valorFinal });
+                
+                // Suma el valor 'amount' de la deuda al valor 'debt' del usuario
+                user.debt += valorFinal;
+
+                // Guarda el usuario actualizado en la base de datos
+                await user.save();
             }
         }
     } catch (error) {
